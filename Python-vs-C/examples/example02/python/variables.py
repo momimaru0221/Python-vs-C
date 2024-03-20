cc0cead89afad630d5a0c3412a882eb56672557c
momimaru0221 <61961041+momimaru0221@users.noreply.github.com>
2024-03-20 10:36:42 +0900

    Add files via upload

---
+++ Python-vs-C/examples/example02/python/variables.py
@@ -0,0 +1,13 @@
+# variables.py
+# このPythonスクリプトは、異なる型の変数を宣言し、それらを出力することで、動的型付けの概念を示します。
+
+integerVar = 42         # 整数
+floatVar = 3.14         # 浮動小数点
+charVar = 'A'           # 文字
+stringVar = "Hello, Python!" # 文字列
+
+# 各変数の値と型を出力
+print(f"Integer: {integerVar}")
+print(f"Float: {floatVar}")
+print(f"Char: {charVar}")
+print(f"String: {stringVar}")
